55f3160b3938de33271762284379b9a137a4722e
Maxsfelp <felipemaxsuel74@gmail.com>
2020-11-16 12:49:09 -0300

    Removendo espaço

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 int main(int argc, char const *argv[]){
 	char comando;
 	system("clear");
-	printf("\n");
 	printf("Sistema de arquivo/comando-> ");
 	scanf("%c", &comando);
 	printf("%c\n", comando);
