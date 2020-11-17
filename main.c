11e86b901639b08839d4ac2eb12ca7c041b6d80e
Maxsfelp <felipemaxsuel74@gmail.com>
2020-11-17 19:55:48 -0300

    inico do shell

--- main.c
+++ main.c
@@ -3,10 +3,18 @@
 #include <string.h>
 
 int main(int argc, char const *argv[]){
-	char comando;
+	char *comando = (char*)malloc(20*sizeof(char));
+	char *caminho = (char*)malloc(200*sizeof(char));
 	system("clear");
-	printf("Sistema de arquivo/comando-> ");
-	scanf("%c", &comando);
-	printf("%c\n", comando);
+	while(1){
+		printf("Fat-> ");
+		scanf("%s %s", comando, caminho);
+		if(strcmp(comando, "init") == 0)
+			printf("%s %s\n", comando, caminho);
+
+		if(strcmp(comando, "quit") == 0)
+			break;
+	}
+
 	return 0;
 }
